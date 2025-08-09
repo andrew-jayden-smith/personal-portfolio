3f3aab69ad9b471ad2d8ee86a69379a0d8469de5
Andrew Smith <smithdrew867@gmail.com>
2025-08-09 15:54:11 -0400

    Loading Screen finished

--- config/application.rb
+++ config/application.rb
@@ -15,6 +15,7 @@ module PersonalPortfolio
     # not contain `.rb` files, or that should not be reloaded or eager loaded.
     # Common ones are `templates`, `generators`, or `middleware`, for example.
     config.autoload_lib(ignore: %w[assets tasks])
+    config.assets.paths << Rails.root.join("app/assets/audios")
 
     # Configuration for the application, engines, and railties goes here.
     #
