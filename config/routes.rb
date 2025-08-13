15a7e12c22cb7d95027a3424b06721a9fd4869d5
Andrew Smith <smithdrew867@gmail.com>
2025-08-13 12:53:13 -0400

    Design Changes

--- config/routes.rb
+++ config/routes.rb
@@ -11,6 +11,7 @@ Rails.application.routes.draw do
 
   # config/routes.rb
   get "/about", to: "pages#about"
+  get "/contact", to: "pages#contact"
 
     # Render dynamic PWA files from app/views/pwa/* (remember to link manifest in application.html.erb)
     # get "manifest" => "rails/pwa#manifest", as: :pwa_manifest
